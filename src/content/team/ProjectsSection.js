d64a6485352b528bf3577e9bffb7806132f64550
Brandon Swenson <bmswens@gmail.com>
2023-03-28 23:50:47 -0500

    add icon buttons to projects

--- src/content/team/ProjectsSection.js
+++ src/content/team/ProjectsSection.js
@@ -4,6 +4,58 @@ import React from 'react'
 // MUI
 import { Divider, Stack, Typography } from '@mui/material'
 
+// MUI Icons
+import ImageIcon from '@mui/icons-material/Image';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LaunchIcon from '@mui/icons-material/Launch';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import LinkIcon from '@mui/icons-material/Link';
+
+// custom
+import LabeledIconButton from './LabeledIconButton';
+
+function ProjectButtons(props) {
+
+    const buttonMappings = [
+        {
+            label: "External Link",
+            key: "external",
+            icon: <LinkIcon fontSize="large" />
+        },
+        {
+            label: "View Image",
+            key: "image",
+            icon: <ImageIcon fontSize="large" />
+        },
+        {
+            label: "View Source",
+            key: "source",
+            icon: <GitHubIcon fontSize="large" />
+        },
+        {
+            label: "Go To Application",
+            key: "demo",
+            icon: <LaunchIcon fontSize="large" />
+        },
+        {
+            label: "View Video",
+            key: "video",
+            icon: <YouTubeIcon fontSize="large" />
+        },
+    ]
+    
+    return (
+        <Stack
+            spacing={1}
+            direction="row"
+            justifyContent="flex-end"
+        >
+            {buttonMappings.map(mapping => <LabeledIconButton label={mapping.label} url={props[mapping.key]} key={props.name + mapping.key}>{mapping.icon}</LabeledIconButton>)}
+        </Stack>
+    )
+}
+
+
 function ProjectInfo(props) {
     const {
         name,
@@ -13,6 +65,7 @@ function ProjectInfo(props) {
         <>
             <Typography variant="h5">{name}</Typography>
             <Typography sx={{ml: 2}}>{description}</Typography>
+            <ProjectButtons {...props} />
         </>
     )
 }
