c94732a8d59aafe41f8e1ebfba9d3c48ead05d52
Brandon Swenson <bmswens@gmail.com>
2023-04-10 16:33:47 -0700

    update tests

---
+++ src/content/team/TeamView.test.js
@@ -0,0 +1,18 @@
+// testing help
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+
+// to test
+import TeamView from "./TeamView"
+
+describe('<TeamView>', function() {
+    it("should allow the user to view other teams", function() {
+        render(<TeamView />)
+        let cohortButton = screen.getByLabelText("Cohort")
+        userEvent.click(cohortButton)
+        let gamma = screen.getByRole("option", { name: "Cohort Gamma"})
+        userEvent.click(gamma)
+        let text = screen.getByText("Outer Space")
+        expect(text).not.toBeNull()
+    })
+})
